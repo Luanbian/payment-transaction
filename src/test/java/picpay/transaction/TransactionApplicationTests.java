c8b6563f4c08a7f01e5927474557f377aa17a593
luan <luanbianchine1@gmail.com>
2023-10-23 18:52:39 -0300

    first commit

---
+++ src/test/java/picpay/transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package picpay.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
