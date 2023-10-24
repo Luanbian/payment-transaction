5242487245155446e28061088c359573074f2949
luan <luanbianchine1@gmail.com>
2023-10-24 12:30:08 -0300

    feat: implements negociation rules to transaction service

--- src/main/java/picpay/transaction/data/services/TransactionService.java
+++ src/main/java/picpay/transaction/data/services/TransactionService.java
@@ -1,9 +1,17 @@
 package picpay.transaction.data.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import picpay.transaction.core.dtos.TransactionDto;
+import picpay.transaction.domain.transactions.Transaction;
 import picpay.transaction.domain.users.User;
 import picpay.transaction.infra.repositories.TransactionRepository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 public class TransactionService {
     @Autowired
@@ -11,10 +19,39 @@ public class TransactionService {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public void create(TransactionDto transactionDto) throws Exception {
         User sender = this.userService.findById(transactionDto.senderId());
         User receiver = this.userService.findById(transactionDto.receiverId());
 
         userService.validateTransaction(sender, transactionDto.value());
+
+        boolean isAuthorized = this.authorizeTransaction(sender, transactionDto.value());
+        if(!isAuthorized) throw new Exception("Transação não autorizada");
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setAmount(transactionDto.value());
+        newTransaction.setSender(sender);
+        newTransaction.setReceiver(receiver);
+        newTransaction.setTimestamp(LocalDateTime.now());
+
+        sender.setBalance(sender.getBalance().subtract(transactionDto.value()));
+        receiver.setBalance(receiver.getBalance().add(transactionDto.value()));
+
+        this.repository.save(newTransaction);
+        this.userService.save(sender);
+        this.userService.save(receiver);
+     }
+
+    public boolean authorizeTransaction(User sender, BigDecimal value) {
+       ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("url", Map.class);
+       if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+           String message = (String) authorizationResponse.getBody().get("message");
+           return "Autorizado".equalsIgnoreCase(message);
+       } else {
+           return false;
+       }
     }
 }
