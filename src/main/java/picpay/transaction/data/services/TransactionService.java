fefb8adf6540e2b08197c6bc3b94228fbeb06c51
luan <luanbianchine1@gmail.com>
2023-10-25 12:52:51 -0300

    refactor: change authorizeTransaction to separate service

--- src/main/java/picpay/transaction/data/services/TransactionService.java
+++ src/main/java/picpay/transaction/data/services/TransactionService.java
@@ -1,19 +1,15 @@
 package picpay.transaction.data.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import picpay.transaction.core.dtos.TransactionDto;
 import picpay.transaction.domain.transactions.Transaction;
 import picpay.transaction.domain.users.User;
 import picpay.transaction.infra.repositories.TransactionRepository;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
+
 @Service
 public class TransactionService {
     @Autowired
@@ -21,7 +17,7 @@ public class TransactionService {
     @Autowired
     private UserService userService;
     @Autowired
-    private RestTemplate restTemplate;
+    private AuthorizationService authorizationService;
     @Autowired
     private NotificationService notificationService;
 
@@ -31,7 +27,7 @@ public class TransactionService {
 
         userService.validateTransaction(sender, transactionDto.value());
 
-        boolean isAuthorized = this.authorizeTransaction(sender, transactionDto.value());
+        boolean isAuthorized = this.authorizationService.authorizeTransaction(sender, transactionDto.value());
         if(!isAuthorized) throw new Exception("Transação não autorizada");
 
         Transaction newTransaction = new Transaction();
@@ -55,15 +51,5 @@ public class TransactionService {
 
     public List<Transaction> getAll() {
         return this.repository.findAll();
-    }
-
-    public boolean authorizeTransaction(User sender, BigDecimal value) {
-       ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
-       if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
-           String message = (String) authorizationResponse.getBody().get("message");
-           return "Autorizado".equalsIgnoreCase(message);
-       } else {
-           return false;
-       }
     }
 }
