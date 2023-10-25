ebb24bab8d7aecc25754320d805c51c84f4fa25c
luan <luanbianchine1@gmail.com>
2023-10-25 12:47:37 -0300

    feat: structure to test transcation service

---
+++ src/test/java/picpay/transaction/data/services/TransactionServiceTest.java
@@ -0,0 +1,20 @@
+package picpay.transaction.data.services;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionServiceTest {
+
+    @Test
+    @DisplayName("Should create transaction when everything is Ok")
+    void createSuccess() {
+
+    }
+
+    @Test
+    @DisplayName("Should throw exception when transaction is not allowed")
+    void createException() {
+    }
+}
